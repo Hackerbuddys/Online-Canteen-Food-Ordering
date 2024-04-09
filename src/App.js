2ec9b15f320fd8512ba451f8a754d8d984bda5a4
Sandeep Gupta~  git config --global user.name Sandeep <sandip.gupta@dbtpl.com>
2024-04-09 15:16:47 +0530

    add review page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Menu from "./components/Menu";
 import Product from "./components/Product";
+import Review from "./components/Review"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Menu />
       <Product />
+      <Review />
     </>
   );
 }
