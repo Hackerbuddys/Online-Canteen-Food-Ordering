2b839a0cfdec9bdf57a72c08cbe743cd5c94ca49
Sandeep Gupta~  git config --global user.name Sandeep <sandip.gupta@dbtpl.com>
2024-04-09 15:08:01 +0530

    added some pictures

--- src/Data.js
+++ src/Data.js
@@ -4,12 +4,18 @@ import menuImage3 from "./assets/images/menu-3.png";
 import menuImage4 from "./assets/images/menu-4.png";
 import menuImage5 from "./assets/images/menu-5.png";
 import menuImage6 from "./assets/images/menu-6.png";
+import menuImage7 from "./assets/images/menu-7.png";
+import menuImage8 from "./assets/images/menu-8.png";
+import menuImage9 from "./assets/images/cart-item-1.png";
+import menuImage10 from "./assets/images/cart-item-2.png";
+import menuImage11 from "./assets/images/cart-item-3.png";
+import menuImage12 from "./assets/images/cart-item-4.png";
 import cartImage1 from "./assets/images/cart-item-1.png";
 import cartImage2 from "./assets/images/cart-item-2.png";
 import cartImage3 from "./assets/images/cart-item-3.png";
 import cartImage4 from "./assets/images/cart-item-4.png";
 import productImage1 from "./assets/images/product-1.png";
-import productImage2 from "./assets/images/product-3.png";
+import productImage2 from "./assets/images/product-2.png";
 import productImage3 from "./assets/images/product-3.png";
 import reviewImage1 from "./assets/images/pic-1.png";
 import reviewImage2 from "./assets/images/pic-1.png";
@@ -20,53 +26,108 @@ import blogImage3 from "./assets/images/blog-3.jpeg";
 const menu = [
   {
     img: menuImage1,
+      // name: "Burgar",
+    // price: "$9.10/-",
   },
   {
     img: menuImage2,
+      // name: "Burgar",
+    // price: "$9.10/-",
   },
   {
     img: menuImage3,
+      // name: "Burgar",
+    // price: "$9.10/-",
   },
   {
     img: menuImage4,
+      // name: "Burgar",
+    // price: "$9.10/-",
   },
   {
     img: menuImage5,
+      // name: "Burgar",
+    // price: "$9.10/-",
   },
   {
     img: menuImage6,
+      // name: "Burgar",
+    // price: "$9.10/-",
   },
+  {
+    img: menuImage7,
+      // name: "Burgar",
+    // price: "$9.10/-",
+  },
+  {
+    img: menuImage8,
+      // name: "Burgar",
+    // price: "$9.10/-",
+  },
+   {
+    img: menuImage9,
+      // name: "Burgar",
+    // price: "$9.10/-",
+  },
+  {
+    img: menuImage10,
+      // name: "Burgar",
+    // price: "$9.10/-",
+  },
+  {
+    img: menuImage11,
+      // name: "Burgar",
+    // price: "$9.10/-",
+  },
+  {
+    img: menuImage12,
+      // name: "Burgar",
+    // price: "$9.10/-",
+  },
+
 ];
 
 const cart = [
   {
     img: cartImage1,
+    // name: 'Chicken Burger',
+    // qty: 3,
+    // price: '$6.50/pc'
   },
   {
     img: cartImage2,
+    // name: 'Chicken Burger',
+    // qty: 3,
+    // price: '$6.50/pc'
   },
   {
     img: cartImage3,
+    // name: 'Chicken Burger',
+    // qty: 3,
+    // price: '$6.50/pc'
   },
   {
     img: cartImage4,
+    // name: 'Chicken Burger',
+    // qty: 3,
+    // price: '$6.50/pc'
   },
 ];
 
 const product = [
   {
     img: productImage1,
-    name: "Product Name",
+    // name: "Burgar",
     // price: "$9.10/-",
   },
   {
     img: productImage2,
-    name: "Product Name",
+    // name: "Combo Ice Cream",
     // price: "$9.10/-",
   },
   {
     img: productImage3,
-    name: "Product Name",
+    // name: "Fresh Ice Cream",
     // price: "$9.10/-",
   },
 ];
