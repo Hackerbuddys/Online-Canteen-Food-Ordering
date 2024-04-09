4632159990c52a062097f4c8da83f8d67534085a
Sandeep Gupta~  git config --global user.name Sandeep <sandip.gupta@dbtpl.com>
2024-04-09 14:31:43 +0530

    about , home menu and profucts page will we done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./assets/CSS/style.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import Product from "./components/Product";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <About />
+      <Menu />
+      <Product />
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,9 @@ const Home = () => {
     <>
       <section className="home" id="home">
         <div className="content">
-          <h1>Hi there!</h1>
+          <h3>Fresh <span>food in the </span>morning</h3>
+          <p>At Fresh Food in the Morning, we believe that starting your day with nutritious and delicious food sets the tone for a healthy and energized day ahead. We are passionate about providing you with the freshest ingredients to kick-start your mornings and fuel your body with goodness.</p>
+          <a href="#" className="btn">get yours now</a>
         </div>
       </section>
     </>
